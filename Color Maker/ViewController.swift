23ba26c4124b19607262ea72551c0d3dcda3f8c1
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2019-05-25 21:32:33 +0300

    Initial Commit

---
+++ Color Maker/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Color Maker
+//
+//  Created by Rinalds Domanovs on 24/05/2019.
+//  Copyright © 2019 Rinalds Domanovs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: Outlets
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    // MARK: Actions
+
+    @IBAction func changeColorComponent(_ sender: AnyObject) {
+
+//        let r: CGFloat = self.redControl.isOn ? 1 : 0
+//        let g: CGFloat = self.greenControl.isOn ? 1 : 0
+//        let b: CGFloat = self.blueControl.isOn ? 1 : 0
+//
+//        colorView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
+    }
+}
+
