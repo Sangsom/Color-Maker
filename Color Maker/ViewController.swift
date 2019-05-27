866e5187b0d687ce2928c01d390387926e34369d
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2019-05-27 10:48:35 +0300

    Updated UI

--- Color Maker/ViewController.swift
+++ Color Maker/ViewController.swift
@@ -15,18 +15,14 @@ class ViewController: UIViewController {
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
     @IBOutlet var colorView: UIView!
-
+    @IBOutlet var hexValueLabell: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         redSlider.minimumTrackTintColor = .red
         greenSlider.minimumTrackTintColor = .green
         blueSlider.minimumTrackTintColor = .blue
-
-        // Do any additional setup after loading the view.
-        colorView.layer.borderWidth = 3
-        colorView.layer.borderColor = UIColor.black.cgColor
-        colorView.layer.cornerRadius = 8
     }
 
     // MARK: Actions
@@ -38,9 +34,66 @@ class ViewController: UIViewController {
         let b: CGFloat = CGFloat(blueSlider.value)
 
         colorView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
+
+        if let hexValue = colorView.backgroundColor?.toHex() {
+            hexValueLabell.text = "#\(hexValue)"
+        }
         
     }
 
-
 }
 
+extension UIColor {
+    // MARK: Initialization
+    convenience init?(hex: String) {
+        var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+        hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
+        var rgb: UInt32 = 0
+        var r: CGFloat = 0.0
+        var g: CGFloat = 0.0
+        var b: CGFloat = 0.0
+        var a: CGFloat = 1.0
+        let length = hexSanitized.count
+        guard Scanner(string: hexSanitized).scanHexInt32(&rgb) else { return nil }
+
+        if length == 6 {
+            r = CGFloat((rgb & 0xFF0000) >> 16) / 255.0
+            g = CGFloat((rgb & 0x00FF00) >> 8) / 255.0
+            b = CGFloat(rgb & 0x0000FF) / 255.0
+        } else if length == 8 {
+            r = CGFloat((rgb & 0xFF000000) >> 24) / 255.0
+            g = CGFloat((rgb & 0x00FF0000) >> 16) / 255.0
+            b = CGFloat((rgb & 0x0000FF00) >> 8) / 255.0
+            a = CGFloat(rgb & 0x000000FF) / 255.0
+        } else {
+            return nil
+        }
+
+        self.init(red: r, green: g, blue: b, alpha: a)
+    }
+
+    func toHex(alpha: Bool = false) -> String? {
+        guard let components = cgColor.components, components.count >= 3 else {
+            return nil
+        }
+
+        let r = Float(components[0])
+        let g = Float(components[1])
+        let b = Float(components[2])
+        var a = Float(1.0)
+
+        if components.count >= 4 {
+            a = Float(components[3])
+        }
+
+        if alpha {
+            return String(format: "%02lX%02lX%02lX%02lX", lroundf(r * 255), lroundf(g * 255), lroundf(b * 255), lroundf(a * 255))
+        } else {
+            return String(format: "%02lX%02lX%02lX", lroundf(r * 255), lroundf(g * 255), lroundf(b * 255))
+        }
+    }
+
+    var toHex: String? {
+        return toHex()
+    }
+}
